d339b10096bb3a19ec96ba3b7a099138aaebcc04
Tomer Vainshtok <tomerv@indeni.com>
2021-06-30 14:57:25 +0300

    pylint fix

--- dragoneye/utils/threading_utils.py
+++ dragoneye/utils/threading_utils.py
@@ -21,21 +21,21 @@ def execute_parallel_functions_in_threads(tasks_groups: Deque[List[ThreadedFunct
     of lists, when each queued item represents a list of functions that should be run in parallel.
     """
     tasks_responses: List[Tuple[Future, str, str]] = []
-    executor: ThreadPoolExecutor = ThreadPoolExecutor(max_workers=max_workers)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
 
-    for tasks_group in tasks_groups:
-        for task in tasks_group:
-            tasks_responses.append((
-                executor.submit(task.callable, *task.args),
-                task.error_msg, task.timeout_msg))
+        for tasks_group in tasks_groups:
+            for task in tasks_group:
+                tasks_responses.append((
+                    executor.submit(task.callable, *task.args),
+                    task.error_msg, task.timeout_msg))
 
-        concurrent.futures.wait([response[0] for response in tasks_responses], timeout=timeout, return_when=ALL_COMPLETED)
+            concurrent.futures.wait([response[0] for response in tasks_responses], timeout=timeout, return_when=ALL_COMPLETED)
 
-    timeout_tasks = [response for response in tasks_responses if response[0].running()]
-    for timeout_task in timeout_tasks:
-        logger.exception(timeout_task[2])
-    failed_tasks = [response for response in tasks_responses if response[0].exception()]
-    for failed_task in failed_tasks:
-        logger.exception(failed_task[1], exc_info=failed_task[0].exception())
-    executor.shutdown(True)
+        timeout_tasks = [response for response in tasks_responses if response[0].running()]
+        for timeout_task in timeout_tasks:
+            logger.exception(timeout_task[2])
+        failed_tasks = [response for response in tasks_responses if response[0].exception()]
+        for failed_task in failed_tasks:
+            logger.exception(failed_task[1], exc_info=failed_task[0].exception())
+
     return tasks_responses
