609947982c9091b6acd51b7960211c7c136d4adb
Tomer Vainshtok <tomerv@indeni.com>
2021-07-06 15:04:19 +0300

    Added missing messages for GcpCredentialsFactory methods

--- dragoneye/cloud_scanner/gcp/gcp_credentials_factory.py
+++ dragoneye/cloud_scanner/gcp/gcp_credentials_factory.py
@@ -49,22 +49,28 @@ class GcpCredentialsFactory:
         :param scopes: The scopes of the created credentials
         :return: An impersonated credentials instance
         """
+        logger.info(f'Will try to generate short-lived credentials for {email}...')
         credentials = impersonated_credentials.Credentials(source_credentials=credentials,
                                                            target_principal=email,
                                                            target_scopes=scopes)
         cls.test_connectivity(credentials)
+        logger.info('Generated credentials successfully')
         return credentials
 
     @classmethod
     def from_aws_credentials_config_info(cls, info: dict):
+        logger.info('Will try to generate short-lived credentials for an AWS workload identity user from credentials config info...')
         cred = aws.Credentials.from_info(info)
         cls.test_connectivity(cred)
+        logger.info('Generated credentials successfully')
         return cred
 
     @classmethod
     def from_aws_credentials_config_file(cls, config_file_path: str):
+        logger.info('Will try to generate short-lived credentials for an AWS workload identity user from credentials config file...')
         cred = aws.Credentials.from_file(config_file_path)
         cls.test_connectivity(cred)
+        logger.info('Generated credentials successfully')
         return cred
 
     @staticmethod
